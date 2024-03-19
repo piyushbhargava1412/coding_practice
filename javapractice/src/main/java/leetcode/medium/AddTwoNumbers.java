59b6154f54a56cf7cbe14d472227dc0fc5180a37
Piyush Bhargava <piyush.bhargava@thoughtworks.com>
2024-03-20 05:06:28 +0530

    Add 2 numbers in linked list

---
+++ javapractice/src/main/java/leetcode/medium/AddTwoNumbers.java
@@ -0,0 +1,111 @@
+package main.java.leetcode.medium;
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        final AddTwoNumbers processor = new AddTwoNumbers();
+        int [] list1 = {9,9,9,9,9,9,9};
+        int [] list2 = {9,9,9,9};
+        System.out.println(processor.addTwoNumbers(createList(list1), createList(list2)));
+    }
+
+    public static ListNode createList(int [] nums) {
+        ListNode head = new ListNode(nums[0], null);
+        ListNode curr = head;
+        for(int i = 1; i < nums.length; i++) {
+            curr.next = new ListNode(nums[i], null);
+            curr = curr.next;
+        }
+        return head;
+    }
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if(l1 == null && l2 != null) return l2;
+        if(l2 == null && l1 != null) return l1;
+        if(l1.val == 0 && l1.next == null && l2 != null) return l2;
+        if(l2.val == 0 && l2.next == null && l1 != null) return l1;
+
+        ListNode output = null;
+        int carryForward = 0;
+        int sum = l1.val + l2.val;
+        if(sum < 10)
+            output = new ListNode(sum, null);
+        else {
+            output = new ListNode(sum % 10, null);
+            carryForward = 1;
+        }
+        ListNode head = output;
+        while (l1.next != null && l2.next != null) {
+            l1 = l1.next;
+            l2 = l2.next;
+            sum = l1.val + l2.val + carryForward;
+            if (sum < 10) {
+                output.next = new ListNode(sum, null);
+                carryForward = 0;
+            } else {
+                output.next = new ListNode(sum % 10, null);
+                carryForward = 1;
+            }
+            output = output.next;
+        }
+
+        l1 = (l1.next == null) ? l2 : l1;
+
+        if(l1.next != null) {
+            while(l1.next != null) {
+                l1 = l1.next;
+                sum = l1.val + carryForward;
+                if (sum < 10) {
+                    output.next = new ListNode(sum, null);
+                    carryForward = 0;
+                } else {
+                    output.next = new ListNode(sum % 10, null);
+                    carryForward = 1;
+                }
+                output = output.next;
+            }
+        }
+//        else if(l2 != null) {
+//            output.next = new ListNode();
+//            output = output.next;
+//            while(l2 != null) {
+//                final int sum = l2.val + carryForward;
+//                if (sum < 10) {
+//                    output.val = sum;
+//                    carryForward = 0;
+//                } else {
+//                    output.val = sum % 10;
+//                    carryForward = 1;
+//                }
+//                if(l2.next != null) {
+//                    output.next = new ListNode();
+//                    output = output.next;
+//                }
+//                l2 = l2.next;
+//            }
+//        }
+        if(carryForward > 0) {
+            output.next = new ListNode(carryForward, null);
+        }
+//        else
+//            output = null;
+
+        return head;
+    }
+}

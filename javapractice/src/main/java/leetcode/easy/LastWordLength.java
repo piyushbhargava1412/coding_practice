8a5cacb4c4e9fbd26811f7149b9037a92a74e8dd
Piyush Bhargava <piyush.bhargava@thoughtworks.com>
2024-04-02 08:30:23 +0530

    Last word length

---
+++ javapractice/src/main/java/leetcode/easy/LastWordLength.java
@@ -0,0 +1,25 @@
+package main.java.leetcode.easy;
+
+public class LastWordLength {
+    public static void main(String[] args) {
+        System.out.println(lengthOfLastWord(" hello"));
+    }
+
+    public static int lengthOfLastWord(String s) {
+        int len = s.length();
+        int i = len - 1;
+        if(s.charAt(i) == ' ') {
+            while(s.charAt(i) == ' ') i--;
+        }
+
+        if(len == 1) return 1;
+
+        int size = 0;
+        while(s.charAt(i) != ' ' && i > 0) {
+            i--;
+            size++;
+        }
+        if(i == 0 && s.charAt(i) != ' ') size++;
+        return size;
+    }
+}
